5082876180f8b3aee48dbed724e4600b69d10e8e
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2024-03-26 19:03:49 +0200

    ...

--- cmd/ping_low.sh
+++ cmd/ping_low.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i dash -I channel:nixos-23.11-small -p dash jq flock
+#! nix-shell -i dash -I channel:nixos-23.11-small -p dash jq flock ookla-speedtest
 set -eu
 
 dash ./cmd/speed.sh ping.low
--- cmd/speed.sh
+++ cmd/speed.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i dash -I channel:nixos-23.11-small -p dash nix jq flock
+#! nix-shell -i dash -I channel:nixos-23.11-small -p dash nix jq flock ookla-speedtest
 set -eu
 
 selector="${1:-}"
 
-NIXPKGS_ALLOW_UNFREE=1 ./speedtest_cached.sh "$(cat .speed-host)" | jq -r ".$selector"
+dash ./speedtest_cached.sh "$(cat .speed-host)" | jq -r ".$selector"
