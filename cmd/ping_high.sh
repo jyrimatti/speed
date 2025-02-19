e06e9ffa9d8cb481722a55dc0ccedb489ba1f0d1
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2025-02-19 18:49:10 +0200

    env var seems to mess path search here

--- cmd/ping_high.sh
+++ cmd/ping_high.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env NIXPKGS_ALLOW_UNFREE=1 nix-shell
+#! /usr/bin/env nix-shell
 #! nix-shell --pure --keep CREDENTIALS_DIRECTORY --keep BKT_CACHE_DIR --keep NIXPKGS_ALLOW_UNFREE
 #! nix-shell -i dash -I channel:nixos-24.11-small -p dash jq flock ookla-speedtest bkt
 set -eu
